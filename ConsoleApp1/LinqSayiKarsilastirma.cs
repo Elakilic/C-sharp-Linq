0a406cb38cce5c18f0f5932e5e30ab6fe8416353
Elakilic <berfinela877@gmail.com>
2023-08-26 17:43:39 +0300

    LinqSayiKarsilastirma.cs dosyası eklendi.

---
+++ ConsoleApp1/LinqSayiKarsilastirma.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class LinqSayiKarsilastirma
+    {
+        public static void Main(string[] args)
+        {
+            int[] sayilar1 = { 1, 3, 5, 7, 9 };
+            int[] sayilar2 = { 1, 5, 8, 7, 22 };
+
+            var kiyas=
+                from bir in sayilar1
+                from iki in sayilar2
+                where bir< iki
+                select new { bir, iki }; //verileri anonim tipe atıyoruz.
+            foreach ( var kys in kiyas ) 
+            {
+                Console.WriteLine("sonuç " + kys.bir +  " < " + kys.iki );
+            }
+            
+        }
+    }
+}

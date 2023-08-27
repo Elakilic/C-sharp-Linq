25ac2dbec99e9f80567e00608d91fb79cde5282c
Elakilic <berfinela877@gmail.com>
2023-08-27 14:07:23 +0300

    LinqtoUppertoLower.cs dosyası eklendi.

---
+++ LinqtoUppertoLower.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class LinqtoUppertoLower
+    {
+        public static void Main()
+        {
+            string[] meyve = { "eLmA", "ArMUt", "mUz", "çİLeK", "ManGO" };
+
+            var yazilis =
+                from m in meyve
+                select new { Buyut = m.ToUpper(), Kucult = m.ToLower() };
+            foreach (var yazi in yazilis)
+            {
+                Console.WriteLine("Büyüt: " +yazi.Buyut + " Küçült: " +yazi.Kucult);
+            }
+        }
+    }
+}

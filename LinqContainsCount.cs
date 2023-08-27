8c9c97cdf02a2c84c8b6ea224f77e08e708d66bf
Elakilic <berfinela877@gmail.com>
2023-08-27 23:29:29 +0300

    Üç yeni dosya eklendi.

---
+++ LinqContainsCount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class LinqContainsCount
+    {
+        public static void Main(string[] args)
+        {
+            int[] sayilar = { 10, 20, 30, 40, 50 };
+
+            var varMi =(from sayi in sayilar select sayi).Contains(70); //dizide 70 var mı varsa true yoksa false
+
+            Console.WriteLine(varMi);
+
+            var kacDeger= sayilar.Count();
+            Console.Write("Değer Sayısı");
+            Console.WriteLine(kacDeger);
+        }
+    }
+}
---
+++ LinqMinandMax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class MinandMax
+    {
+        public static void Main()
+        {
+            int[] sayilar = { 5, 35, 45, 98, 78, 1, 63, 72 };
+
+            var MinSayi = sayilar.Min();
+            Console.WriteLine("Minimum Sayı: " + MinSayi);
+
+            var MaxSayi = sayilar.Max();
+            Console.WriteLine("Maximum Sayı: " + MaxSayi);
+
+
+        }
+        
+
+
+    }
+}

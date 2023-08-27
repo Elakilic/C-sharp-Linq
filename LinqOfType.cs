a9a5d7fcd13e3d53fee38952c34a77e7cd84eedb
Elakilic <berfinela877@gmail.com>
2023-08-27 16:55:10 +0300

    LinqOfType.cs dosyası eklendi.

---
+++ LinqOfType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class LinqOfType
+    {
+        public static void Main(string[] args)
+        {
+            object[] objects = { "bir ", "iki", 3.3, 4.6, "pazar", 78 };
+            
+            var doubleSayilar= objects.OfType<double>();
+            Console.WriteLine("objects dizisindeki double sayılar: ");
+            foreach(var i  in doubleSayilar)
+            {
+                Console.WriteLine(i);
+            }
+            var intSayilar= objects.OfType<int>();
+            Console.WriteLine("objects dizisindeki integer sayılar: ");
+            foreach (var i in intSayilar)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+    }
+}

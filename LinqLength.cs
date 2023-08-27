bf126496d55072dd14a8f368db2c7f642cb91b58
Elakilic <berfinela877@gmail.com>
2023-08-27 15:17:27 +0300

    LinqLength.cs dosyası eklendi.

---
+++ LinqLength.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class LinqLength
+    {
+        public static void Main(string[] args)
+        {
+            string[] sehirler = { "diyarbakır", "adana", "antalya", "hatay", "muş", "bolu" };
+
+            var liste =
+                from s in sehirler
+                orderby s.Length
+                select s;
+            Console.WriteLine("Uzunluğuna göre listeli şehirler: ");
+            foreach (var s in liste)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}

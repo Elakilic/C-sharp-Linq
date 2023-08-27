180917f46f1b189f686e55f56fdc1f000df50149
Elakilic <berfinela877@gmail.com>
2023-08-27 23:44:49 +0300

    2 yeni dosya eklendi.

---
+++ LinqAverage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class LinqAverage
+    {
+        public static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 2, 6, 9, 23, 45, 56, 78 };
+
+            double ortalama= list.Average();
+
+            Console.WriteLine("Ortalama: "+ortalama);
+        }
+    }
+}
---
+++ LinqConcat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class LinqConcat
+    {
+        public static void Main(string[] args) 
+        {
+            List<int> list1 = new List<int>() { 1, 4, 6, 8, 12, 15 };
+            List<int> list2 = new List<int>() { 3, 5, 7, 9, 11, 13 };
+
+            var sayilarım = list1.Concat(list2);
+
+            foreach(var i in sayilarım)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+        }   
+    }
+}
